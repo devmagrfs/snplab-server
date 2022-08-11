18cfa3493006a91f1cb9146e02e94331e809247f
devmagrfs <84789580+devmagrfs@users.noreply.github.com>
2022-08-11 13:46:21 +0900

    fix: post 에러

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ server.use(middlewares);
 
 server.use(jsonServer.bodyParser);
 
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
 server.use(router);
 
 server.listen(5000, () => {
